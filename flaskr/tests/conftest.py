76debe3de42e4729a3a086d31ca27afd183aa98d
densuke5G <danie1.apcb6160@gmail.com>
2024-09-04 22:36:55 +0900

    テストわかんない

---
+++ flaskr/tests/conftest.py
@@ -0,0 +1,40 @@
+import os
+import tempfile
+
+import pytest
+from flaskr import create_app
+from flaskr.db import Session
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        # db.init_db()
+        db_session = Session()
+        db_session.execute(_data_sql)
+        db_session.commit()
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
